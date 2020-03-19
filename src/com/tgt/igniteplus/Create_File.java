9e7cc1f74e5c5fd0049ab4badaaa075f056c7a12
PratilipiAich <pratilipiaich@gmail.com>
2020-03-19 21:58:23 +0530

    Added Create_File.java

---
+++ src/com/tgt/igniteplus/Create_File.java
@@ -0,0 +1,25 @@
+package com.tgt.igniteplus;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Create_File {
+    public static void main(String[] args) {
+        try {
+            File obj = new File("file.txt");
+            if (obj.createNewFile()) {
+                System.out.println("File created: " + obj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+/*
+File created: file.txt
+
+File already exists.
+ */
