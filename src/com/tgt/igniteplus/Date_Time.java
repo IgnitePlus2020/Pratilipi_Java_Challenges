94b87386f64b70946109b5651cbe5024fa30cc6c
PratilipiAich <pratilipiaich@gmail.com>
2020-03-19 14:24:49 +0530

    Added Date_Time.java

---
+++ src/com/tgt/igniteplus/Date_Time.java
@@ -0,0 +1,13 @@
+package com.tgt.igniteplus;
+
+import java.util.Date;
+
+public class Date_Time {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Date and time: \t" + date);
+    }
+}
+/*
+Date and time: 	Thu Mar 19 14:20:04 IST 2020
+ */
